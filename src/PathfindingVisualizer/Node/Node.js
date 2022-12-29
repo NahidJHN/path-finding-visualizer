9fb14038030aa21634bc4edb032003cce82ec810
NahidJHN <jubaidhossain66@gmail.com>
2022-12-29 09:29:56 +0600

    dynamic selection implemented

--- src/PathfindingVisualizer/Node/Node.js
+++ src/PathfindingVisualizer/Node/Node.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import './Node.css';
+import React, { Component } from "react";
+import "./Node.css";
 
 export default class Node extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {};
-    // }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {};
+  // }
 
-    render() {
-        const { col, row, isFinish, isStart, isVisited } = this.props;
-        //tarnary operator with if else
-        const extraClassName = isFinish
-            ? 'node-finish'
-            : isStart
-                ? 'node-start'
-                : isVisited
-                    ? 'node-visited'
-                    : '';
-                    
-        return <div
-            id={`node-${row}-${col}`}
-            className={`node ${extraClassName}`}
-        ></div>;
-    }
+  render() {
+    const { col, row, isFinish, isStart, isVisited, onClickFunc } = this.props;
+    //tarnary operator with if else
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isVisited
+      ? "node-visited"
+      : "";
+
+    return (
+      <div
+        onClick={() => onClickFunc(col, row)}
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+      ></div>
+    );
+  }
 }
 
 export const DEFAULT_NODE = {
-    row: 0,
-    col: 0,
+  row: 0,
+  col: 0,
 };
